cb9bc4747be36b1995a44c12401a0119a90c0c8f
val8119 <mikk.vahaste@gmail.com>
2020-06-23 18:04:14 +0300

    Centered container, arceus on load

--- sandbox/poke_images/assets/js/index.js
+++ sandbox/poke_images/assets/js/index.js
@@ -4,6 +4,27 @@ document.getElementById("search-box").addEventListener('keypress', function (e) 
     }
 });
 
+window.addEventListener("load", function () {
+    var request = new XMLHttpRequest();
+
+    request.open("GET", "https://pokeapi.co/api/v2/pokemon/arceus", true);
+
+    request.onload = function () {
+        if (request.status == 200) {
+
+            var data = JSON.parse(this.response);
+            document.getElementById("name").innerHTML = data.name;
+            document.getElementById("sprite").src = data.sprites.front_default;
+            document.getElementById("HIDDEN").id = "pokemon-container";
+            document.getElementsByName("search-box-1")[0].placeholder = "arceus";
+        } else {
+            document.getElementsByName("search-box-1")[0].placeholder = "invalid";
+        }
+    }
+
+    request.send();
+});
+
 function sendAPIRequest() {
     var searchBox = document.getElementById("search-box");
 
